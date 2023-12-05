79dc97463c242d7d2a209c86a5ccab8d6ba4ca6a
Emma Karpets <e.karpets.job@gmail.com>
2023-12-05 22:23:10 +0200

    adde minor updates

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const text = `The aim of the Lemme project is to introduce and develop the usage of formal methods to construct safe software. We particularlyparticularlyparticularlyparticularlyparticularlyparticularlyv address scientific computing software (computer algebra, computer arithmetics) and software for smart cards. `;  
-
 const repeatedLetter = (input) => {
   const arrayFromInput = input.toLowerCase().replace(/[^a-z0-9]/ig, '').split('');
   const uniqueLetters = [...new Set(arrayFromInput)];
@@ -19,4 +17,4 @@ const repeatedLetter = (input) => {
   return theMostRepeatedLetter;
 }
 
-console.log(repeatedLetter(text))
+module.exports = repeatedLetter;
